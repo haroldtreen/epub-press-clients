1fba88ccd90c18599cf9847af0f13a573ca7eb3a
Harold Treen <haroldtreen@gmail.com>
2016-02-27 14:00:36 -0500

    Latest changes

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -30,7 +30,21 @@ window.onload = () => {
         });
         list.push('</ul>');
         $('#c-tabs-list').append(list.join(''));
-        console.log(tabs);
+
+        var urlss = tabs.map((tab) => {
+            return tab.url;
+        }).filter((url) => {
+            return url.indexOf('http') > -1;
+        });
+
+        $.post('http://localhost:3000/api/books/publish', { 'urls': urlss }, (response) => {
+            console.log(response.bookId);
+            $('#c-tabs-list').append('<h1>Response: ' + JSON.stringify(response) + '</h1>');
+
+            chrome.downloads.download({ url: 'http://localhost:3000/api/books/download?bookId=' + response.bookId }, () => {
+                $('#c-tabs-list').append('<h1>SUCCESS</h1>');
+            });
+        });
     }).catch(function(error) {
         console.log(error);
         console.log(':\'(');
