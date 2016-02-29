b20549d20a6d534d5f1559f7e391c74a3dc492d1
Harold Treen <haroldtreen@gmail.com>
2016-02-28 23:46:27 -0500

    Add buttons and checkboxes to dropdown

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,12 +1,9 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
-
-var getCurrentWindowTabs = () => {
-    console.log('in');
+const getCurrentWindowTabs = () => {
+    const djs= 'd';
     return new Promise((resolve, reject) => {
         chrome.windows.getCurrent((currentWindow) => {
-            console.log(currentWindow);
             chrome.tabs.getAllInWindow(currentWindow.id, (tabs) => {
                 if (tabs) {
                     resolve(tabs);
@@ -18,34 +15,46 @@ var getCurrentWindowTabs = () => {
     });
 };
 
+const getCheckbox = (title, url) => {
+    const html = `<div class="checkbox">
+                  <label>
+                  <input class='article-checkbox' type="checkbox" value="${url}">${title}
+                  </label>
+                  </div>`;
+    return html;
+};
+
+$('#select-all').click(() => {
+    console.log('all');
+    $('input.article-checkbox').each((index, checkbox) => {
+        $(checkbox).prop('checked', true);
+    });
+});
+
+$('#select-none').click(() => {
+    $('input.article-checkbox').each((index, checkbox) => {
+        $(checkbox).prop('checked', false);
+    });
+});
+
 window.onload = () => {
     console.log('opened');
-    getCurrentWindowTabs().then(function(tabs) {
-        var list = ['<ul>'];
-        tabs.forEach((tab) => {
-            var item = ['<li>'];
-            item.push('Title: ' + tab.title + ', Url: ' + tab.url);
-            item.push('</li>');
-            list.push(item.join(''));
-        });
-        list.push('</ul>');
-        $('#c-tabs-list').append(list.join(''));
-
-        var urlss = tabs.map((tab) => {
-            return tab.url;
-        }).filter((url) => {
-            return url.indexOf('http') > -1;
+    getCurrentWindowTabs().then((tabs) => {
+        tabs.filter((tab) => {
+            return tab.url.indexOf('http') > -1;
+        }).forEach((tab) => {
+            $('#tab-list').append(getCheckbox(tab.title, tab.url));
         });
 
-        $.post('http://localhost:3000/api/books/publish', { 'urls': urlss }, (response) => {
-            console.log(response.bookId);
-            $('#c-tabs-list').append('<h1>Response: ' + JSON.stringify(response) + '</h1>');
-
-            chrome.downloads.download({ url: 'http://localhost:3000/api/books/download?bookId=' + response.bookId }, () => {
-                $('#c-tabs-list').append('<h1>SUCCESS</h1>');
-            });
-        });
-    }).catch(function(error) {
+        // $.post('http://localhost:3000/api/books/publish', { 'urls': urlss }, (response) => {
+        //     console.log(response.bookId);
+        //     $('#c-tabs-list').append('<h1>Response: ' + JSON.stringify(response) + '</h1>');
+        //
+        //     chrome.downloads.download({ url: 'http://localhost:3000/api/books/download?bookId=' + response.bookId }, () => {
+        //         $('#c-tabs-list').append('<h1>SUCCESS</h1>');
+        //     });
+        // });
+    }).catch((error) => {
         console.log(error);
         console.log(':\'(');
     });
