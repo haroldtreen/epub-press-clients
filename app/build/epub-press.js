8929e902fb21e762a8ddba1093e387fe5caa4ce2
Harold Treen <haroldtreen@gmail.com>
2016-05-01 22:04:14 -0500

    Move all api requests to EpubPress object.

--- app/build/epub-press.js
+++ app/build/epub-press.js
@@ -1,8 +1,12 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Browser = window.Browser;
+var MANIFEST = Browser.getManifest();
+var BASE_URL = Browser.baseUrl();
 
 var EpubPress = function () {
     function EpubPress() {
@@ -10,7 +14,7 @@ var EpubPress = function () {
     }
 
     _createClass(EpubPress, null, [{
-        key: "isValidUrl",
+        key: 'isValidUrl',
         value: function isValidUrl(url) {
             var matchesValid = true;
             var matchesInvalid = false;
@@ -28,9 +32,77 @@ var EpubPress = function () {
             return matchesValid && !matchesInvalid;
         }
     }, {
-        key: "filterUrls",
+        key: 'filterUrls',
         value: function filterUrls(urls) {
             return (urls || []).filter(EpubPress.isValidUrl);
+        }
+    }, {
+        key: 'compareVersion',
+        value: function compareVersion(versionData) {
+            var apiSupported = Number(versionData.minCompatible.replace('.', ''));
+            var currentVersion = Number(MANIFEST.version.replace('.', ''));
+
+            if (apiSupported > currentVersion) {
+                if (versionData.message) {
+                    $('#status-server').text(versionData.message);
+                }
+            }
+        }
+    }, {
+        key: 'checkForUpdates',
+        value: function checkForUpdates() {
+            $.ajax({
+                url: BASE_URL + '/api/version',
+                contentType: 'application/json'
+            }).done(function (versionData) {
+                console.log(versionData);
+                EpubPress.compareVersion(versionData);
+            }).fail(function (xhr, err) {
+                console.error('Version Check Failed:');
+                console.error(err);
+            });
+        }
+    }, {
+        key: 'requestEbook',
+        value: function requestEbook(sections) {
+            return new Promise(function (resolve, reject) {
+                $.ajax({
+                    url: BASE_URL + '/api/books',
+                    method: 'POST',
+                    data: JSON.stringify({ sections: sections }),
+                    contentType: 'application/json'
+                }).done(function (response) {
+                    console.log(response);
+                    resolve(response.id);
+                }).fail(function (xhr, err) {
+                    console.log(err);
+                    reject(err);
+                }).always(function (xhr, status, err) {
+                    console.log(status);
+                    console.log(err);
+                });
+            });
+        }
+    }, {
+        key: 'downloadEbook',
+        value: function downloadEbook(params) {
+            return new Promise(function (resolve, reject) {
+                if (params.id) {
+                    var queryString = $.param(params);
+                    var url = BASE_URL + '/api/books/download?' + queryString;
+
+                    if (params.email && params.email.length > 0) {
+                        $.ajax({ url: url }).done(function (response) {
+                            console.log(response);
+                            resolve();
+                        }).fail(function (xhr, err) {
+                            reject(err);
+                        });
+                    } else {
+                        Browser.download({ url: url }, resolve);
+                    }
+                }
+            });
         }
     }]);
 
--- app/scripts/epub-press.js
+++ app/scripts/epub-press.js
@@ -1,3 +1,7 @@
+const Browser = window.Browser;
+const MANIFEST = Browser.getManifest();
+const BASE_URL = Browser.baseUrl();
+
 class EpubPress {
     static isValidUrl(url) {
         let matchesValid = true;
@@ -18,6 +22,70 @@ class EpubPress {
 
     static filterUrls(urls) {
         return (urls || []).filter(EpubPress.isValidUrl);
+    }
+
+    static compareVersion(versionData) {
+        const apiSupported = Number(versionData.minCompatible.replace('.', ''));
+        const currentVersion = Number(MANIFEST.version.replace('.', ''));
+
+        if (apiSupported > currentVersion) {
+            if (versionData.message) {
+                $('#status-server').text(versionData.message);
+            }
+        }
+    }
+
+    static checkForUpdates() {
+        $.ajax({
+            url: `${BASE_URL}/api/version`,
+            contentType: 'application/json',
+        }).done((versionData) => {
+            console.log(versionData);
+            EpubPress.compareVersion(versionData);
+        }).fail((xhr, err) => {
+            console.error('Version Check Failed:');
+            console.error(err);
+        });
+    }
+
+    static requestEbook(sections) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `${BASE_URL}/api/books`,
+                method: 'POST',
+                data: JSON.stringify({ sections }),
+                contentType: 'application/json',
+            }).done((response) => {
+                console.log(response);
+                resolve(response.id);
+            }).fail((xhr, err) => {
+                console.log(err);
+                reject(err);
+            }).always((xhr, status, err) => {
+                console.log(status);
+                console.log(err);
+            });
+        });
+    }
+
+    static downloadEbook(params) {
+        return new Promise((resolve, reject) => {
+            if (params.id) {
+                const queryString = $.param(params);
+                const url = `${BASE_URL}/api/books/download?${queryString}`;
+
+                if (params.email && params.email.length > 0) {
+                    $.ajax({ url }).done((response) => {
+                        console.log(response);
+                        resolve();
+                    }).fail((xhr, err) => {
+                        reject(err);
+                    });
+                } else {
+                    Browser.download({ url }, resolve);
+                }
+            }
+        });
     }
 }
 
