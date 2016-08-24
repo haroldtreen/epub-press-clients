c83796a36ac11e1b93e0d2f30bda9560f72317be
Harold Treen <haroldtreen@gmail.com>
2016-08-23 22:14:55 -0700

    Switch background.js to use epub-press-js

--- packages/epub-press-chrome/app/scripts/background.js
+++ packages/epub-press-chrome/app/scripts/background.js
@@ -1,31 +1,36 @@
-(function (global) {
-    const Browser = global.Browser;
-    const EpubPress = global.EpubPress;
+import EpubPress from 'epub-press-js';
+import Browser from './browser';
 
-    Browser.onForegroundMessage((request) => {
-        if (request.action === 'download') {
-            Browser.setLocalStorage({ downloadState: true });
-            Browser.getLocalStorage(['email', 'filetype']).then((state) => {
-                EpubPress
-                    .requestEbook(request.book)
-                    .then((id) => { // eslint-disable-line
-                        return EpubPress.downloadEbook({
-                            id,
-                            filetype: state.filetype,
-                            email: state.email,
-                        });
-                    })
-                    .then(() => {
-                        Browser.setLocalStorage({ downloadState: false });
-                        Browser.sendMessage(null, { action: 'download', status: 'complete' });
-                    })
-                    .catch((e) => {
-                        console.log(`Error: ${e}`);
-                        Browser.setLocalStorage({ downloadState: false });
-                        Browser.sendMessage(null,
-                            { action: 'download', status: 'failed', error: e });
-                    });
+const defaultBase = EpubPress.BASE_URL;
+
+// ['BASE_URL', 'PUBLISH_URL', 'DOWNLOAD_URL', 'VERSION_URL'].forEach((url) => {
+//     EpubPress[url] = EpubPress[url] && EpubPress[url].replace(defaultBase, 'http://localhost:3000');
+// });
+
+Browser.onForegroundMessage((request) => {
+    if (request.action === 'download') {
+        Browser.setLocalStorage({ downloadState: true });
+        Browser.getLocalStorage(['email', 'filetype']).then((state) => {
+            const book = new EpubPress(Object.assign({}, request.book, {
+                email: state.email,
+                filetype: state.filetype,
+            }));
+            book
+            .publish()
+            .then(() => { // eslint-disable-line
+                return book.download();
+            })
+            .then(() => {
+                Browser.setLocalStorage({ downloadState: false });
+                Browser.sendMessage(null, { action: 'download', status: 'complete' });
+            })
+            .catch((e) => {
+                Browser.setLocalStorage({ downloadState: false });
+                Browser.sendMessage(
+                    null,
+                    { action: 'download', status: 'failed', error: e.message }
+                );
             });
-        }
-    });
-}(window));
+        });
+    }
+});
