ae9a3f4f0ec9d0b0733d4c9167607f3fab787b35
Harold Treen <haroldtreen@gmail.com>
2016-08-23 22:13:22 -0700

    Webpack backend js.

---
+++ packages/epub-press-chrome/webpack.config.js
@@ -0,0 +1,83 @@
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+
+const MODULE_LOADERS = [
+    {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loader: 'json-loader',
+    },
+    {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+    },
+];
+
+let WebpackConfig;
+if (process.env.ENV !== 'test') {
+    WebpackConfig = {
+        devtool: 'inline-source-map',
+        entry: {
+            popup: ['whatwg-fetch', './app/scripts/popup.js'],
+            background: ['whatwg-fetch', './app/scripts/background.js'],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.join(__dirname, 'app', 'build'),
+        },
+        module: {
+            loaders: MODULE_LOADERS,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            }),
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+            }),
+        ],
+        resolve: {
+            extensions: ['', '.js'],
+        },
+        devServer: {
+            hostname: 'localhost',
+            port: '5000',
+            inline: true,
+        },
+        node: {
+            fs: 'empty',
+        }
+    };
+} else {
+    WebpackConfig = {
+        entry: ['fetch-mock', 'mocha!./tests/index.js'],
+        output: {
+            filename: 'test.build.js',
+            path: path.join(__dirname, 'tests'),
+        },
+        module: {
+            loaders: MODULE_LOADERS,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.ENV': JSON.stringify(process.env.NODE_ENV || 'test')
+            }),
+        ],
+        resolve: {
+            extensions: ['', '.js'],
+        },
+        devServer: {
+            hostname: 'localhost',
+            port: '5001',
+            inline: true,
+        },
+        node: {
+            fs: 'empty',
+        },
+    };
+}
+
+module.exports = WebpackConfig;
